13f860102e1f8a6c400f59086a261bed7dc79b13
superzaky <superkytoz@gmail.com>
2026-03-15 17:26:03 +0000

    fix sound and screens

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-pkill -q polybar
+pkill -x polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar named "example"
-polybar example &
+# Launch bars for all connected monitors
+if type "xrandr" > /dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
