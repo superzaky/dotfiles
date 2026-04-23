810e47f4a6093832c5fa29c8fb840144a553782e
superzaky <superkytoz@gmail.com>
2026-04-24 01:43:22 +0200

    add xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,13 +1,26 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Hooks.EwmhDesktops (ewmh)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks (docks, avoidStruts)
+import XMonad.Util.Run (spawnPipe)
+import System.IO (hPutStrLn)
 
 main :: IO ()
-main = xmonad $ ewmh def
-    { terminal           = "alacritty"
-    , modMask            = mod4Mask
-    , borderWidth        = 2
-    } `additionalKeysP`
-    [ ("M-S-w", kill)
-    , ("M-p",   spawn "dmenu_run")
-    ]
+main = do
+    -- This launches xmobar using the system path
+    xmproc <- spawnPipe "xmobar ~/.config/xmobar/.xmobarrc" 
+    xmonad $ docks $ ewmh def
+        { terminal           = "alacritty"
+        , modMask            = mod4Mask -- rebind mod to super key
+        , borderWidth        = 2
+        , layoutHook         = avoidStruts $ layoutHook def -- This stops windows from covering the bar
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle  = xmobarColor "#2ecc71" "" . shorten 50
+            }
+        } `additionalKeysP`
+        [ ("M-S-w",      kill)
+        , ("M-p",        spawn "dmenu_run")
+        , ("M-<Return>", spawn "alacritty")
+        ]
