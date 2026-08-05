5517e29c37025f9afcfb2a6c7461ad9eb2f8a587
superzaky <superkytoz@gmail.com>
2026-08-06 01:07:43 +0200

    edit config

--- emacs/init.el
+++ emacs/init.el
@@ -404,3 +404,6 @@ install if missing on Windows or Unix."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; je hebt 'upcase-region' niet meer laten disablen toen je het benaderde via M-x
+(put 'upcase-region 'disabled nil)
